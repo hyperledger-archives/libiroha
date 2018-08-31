a60cfa64ae9289164449109514d3a871d5ab8d40
Alexey Rodionov <rodionov@soramitsu.co.jp>
2018-08-31 11:14:39 +0300

    fix md5, sha256 and asc
    Signed-off-by: Alexey Rodionov <rodionov@soramitsu.co.jp>

--- .jenkinsci/artifacts.groovy
+++ .jenkinsci/artifacts.groovy
@@ -1,10 +1,8 @@
 #!/usr/bin/env groovy
 
-def uploadArtifacts(filePath, artifactServers=['artifact.soramitsu.co.jp']) {
-  def filePathsConverted = []
+def uploadArtifacts(filePath, artifactServers=['nexus.iroha.tech']) {
+  
   agentType = sh(script: 'uname', returnStdout: true).trim()
-  fp = sh(script: "find ${filePath}/* -type f -print | tr '\n' ','", returnStdout: true).trim()
-  filePathsConverted.addAll(fp.split(','))
 
   def shaSumBinary = 'sha256sum'
   def md5SumBinary = 'md5sum'
@@ -19,16 +17,14 @@ def uploadArtifacts(filePath, artifactServers=['artifact.soramitsu.co.jp']) {
     if (!agentType.contains('MSYS_NT')) {
       sh "gpg --yes --batch --no-tty --import ${CI_GPG_PRIVKEY} || true"
     }
-    filePathsConverted.each {
-      sh "$shaSumBinary ${it} | cut -d' ' -f1 > \$(pwd)/\$(basename ${it}).sha256"
-      sh "$md5SumBinary ${it} | cut -d' ' -f1 > \$(pwd)/\$(basename ${it}).md5"
-      sh "echo \"${CI_GPG_MASTERKEY}\" | $gpgKeyBinary -o \$(pwd)/\$(basename ${it}).asc ${it}"
-    }
+    sh(script: "find ${filePath}/* -type f -exec sh -c \"${shaSumBinary} | cut -d' ' -f1 > {}.md5\" \\;")
+    sh(script: "find ${filePath}/* -type f -exec sh -c \"${md5SumBinary} {} | cut -d' ' -f1 > {}.md5\" \\;")
+    sh(script: "find ${filePath}/* -type f -exec sh -c \"echo \"${CI_GPG_MASTERKEY}\" | $gpgKeyBinary -o {}.asc {}\" \\;")
   }
 
 withCredentials([usernamePassword(credentialsId: 'ci_nexus', passwordVariable: 'NEXUS_PASS', usernameVariable: 'NEXUS_USER')]) {
   artifactServers.each {
-      sh(script: "find ${filePath}/* -type f -exec curl -u ${NEXUS_USER}:${NEXUS_PASS} --upload-file {} https://nexus.iroha.tech/repository/artifacts/{} \\;", reutrnStdout: true)
+      sh(script: "cd ${filePath} && find * -type f -exec curl -u ${NEXUS_USER}:${NEXUS_PASS} --upload-file {} https://nexus.iroha.tech/repository/artifacts/libiroha/{} \\;", reutrnStdout: true)
     }
   }
 }
